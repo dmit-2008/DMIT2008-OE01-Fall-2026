ba5a7667093274e7e31f7ec734f68166fbfaf9e4
Jhimlie Eustaquio <jhimlieE@nait.ca>
2026-09-21 17:44:34 -0600

    Adding catch on .then

--- src/A/review-astronauts-example-START/js/api/astronaut.js
+++ src/A/review-astronauts-example-START/js/api/astronaut.js
@@ -3,7 +3,9 @@ const BASE_URL = "https://lldev.thespacedevs.com/2.2.0"
 // api functions here.
 const getAstronautList = function() {
     return fetch(BASE_URL + '/astronaut/')
-            .then(response => response.json());
+            .then(response => response.json())
+            // TO DO: add error handling here
+            .catch(error => console.error("Caught an eror:", error.message));
 }
 
 export { getAstronautList }
